117ad879dee0c5882a3d79985019b28c1075a520
Chase_55 <14172331+chase55@user.noreply.gitee.com>
2025-04-13 21:31:17 +0800

    feat: 增添微信,qq,支付宝三种登录样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,19 +2,39 @@
   <div class="login-container">
     <el-card class="login-card">
       <h2>用户登录</h2>
-      <el-form :model="loginForm" :rules="rules" ref="loginForm">
-        <el-form-item prop="username">
-          <el-input v-model="loginForm.username" placeholder="用户名"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input v-model="loginForm.password" type="password" placeholder="密码"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm">登录</el-button>
-          <el-button @click="$router.push('/register')">注册</el-button>
-          <el-link type="primary" @click="$router.push('/reset-password')">忘记密码?</el-link>
-        </el-form-item>
-      </el-form>
+      <div class="login-content">
+        <!-- 左侧：第三方登录 -->
+        <div class="login-left">
+          <div class="third-party-login">
+            <el-button class="login-btn qq" icon="el-icon-qq" @click="thirdPartyLogin('qq')">QQ登录</el-button>
+            <el-button class="login-btn wechat" icon="el-icon-wechat" @click="thirdPartyLogin('wechat')">微信登录</el-button>
+            <el-button class="login-btn alipay" icon="el-icon-bank-card" @click="thirdPartyLogin('alipay')">支付宝登录</el-button>
+          </div>
+        </div>
+
+        <!-- 右侧：账号密码登录 -->
+        <div class="login-right">
+          <el-form :model="loginForm" :rules="rules" ref="loginForm">
+            <el-form-item prop="username">
+              <el-input v-model="loginForm.username" placeholder="用户名"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input v-model="loginForm.password" type="password" placeholder="密码"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm" style="width: 100%">登录</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+
+      <!-- 底部链接 -->
+      <div class="login-footer">
+        <el-link type="primary" @click="$router.push('/register')">注册用户</el-link>
+      <!--这里用的是分割线而不是border-->
+        <el-divider direction="vertical"></el-divider>
+        <el-link type="primary" @click="$router.push('/reset-password')">修改密码</el-link>
+      </div>
     </el-card>
   </div>
 </template>
@@ -45,6 +65,10 @@ export default {
           this.$message.success('登录成功')
         }
       })
+    },
+    thirdPartyLogin (type) {
+      this.$message(`正在跳转${type}登录...`)
+      // 这里添加第三方登录逻辑
     }
   }
 }
@@ -59,17 +83,65 @@ export default {
   background-color: white;
 }
 .login-card {
-  width: 400px;
-  padding: 20px;
+  width: 50%;  /* 增加宽度 */
+  padding: 30px;
 }
 h2 {
   text-align: center;
-  margin-bottom: 20px;
+  margin-bottom: 30px;
+  color: #333;
 }
-.el-button + .el-button {
-  margin-left: 10px;
+
+.login-content {
+  display: flex;
+  justify-content: space-between;
 }
-.el-link {
-  margin-left: 10px;
+
+.login-left {
+  width: 48%;
+  padding-right: 20px;
+  /*分割线是怎么来的?相当于加了个右边框*/
+  border-right: 1px solid #eee;
+}
+
+.login-right {
+  width: 48%;
+  padding-left: 20px;
+}
+
+.third-party-login {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: start;
+}
+
+.login-btn {
+  margin-bottom: 15px;
+  width: 100%;
+}
+.login-btn.qq {
+  margin-left:10px;/*补丁*/
+  background-color: #12B7F5;
+  color: white;
+}
+.login-btn.wechat {
+  background-color: #07C160;
+  color: white;
+}
+.login-btn.alipay {
+  background-color: #1677FF;
+  color: white;
+}
+
+.login-footer {
+  margin-top: 20px;
+  text-align: center;
+  padding-top: 20px;
+  border-top: 1px solid #eee;
+}
+
+.el-divider--vertical {
+  margin: 0 10px;
 }
 </style>
